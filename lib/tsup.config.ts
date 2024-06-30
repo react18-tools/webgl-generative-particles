e8a481392e33faf719f216315dace3753b17d19f
Mayank <mayank.srmu@gmail.com>
2024-06-30 07:33:26 +0530

    load shaders

--- lib/tsup.config.ts
+++ lib/tsup.config.ts
@@ -1,3 +1,4 @@
+import fs from "node:fs";
 import { defineConfig, type Options } from "tsup";
 import react18Plugin from "esbuild-plugin-react18";
 import cssPlugin from "esbuild-plugin-react18-css";
@@ -14,6 +15,34 @@ export default defineConfig(
       bundle: true,
       minify: !options.watch,
       esbuildPlugins: [
+        {
+          name: "webgl",
+          setup(build) {
+            if (!options.watch)
+              build.onLoad({ filter: /utils\.ts$/, namespace: "file" }, args => {
+                const text = fs.readFileSync(args.path, "utf8");
+                const contents = text
+                  .replace(/if \(!gl[^}]*}/gm, "")
+                  .replace(/;\s*if \(![^;]*;/gm, "!;")
+                  .trim();
+                return { contents, loader: "ts" };
+              });
+            // eslint-disable-next-line prefer-named-capture-group -- ok
+            build.onLoad({ filter: /\.glsl$/, namespace: "file" }, args => {
+              const text = fs.readFileSync(args.path, "utf8");
+              const lines = text
+                // remove comments
+                .replace(/\/\*.*\*\//gm, "")
+                // remove white spaces around =
+                // .replace(/ = /g, "=")
+                .split("\n")
+                .map(line => line.trim())
+                .filter(Boolean);
+              const contents = `export default \`${lines[0]}\n${lines.slice(1).join("")}\``;
+              return { contents, loader: "ts" };
+            });
+          },
+        },
         react18Plugin({ disableJSXRequireDedup: true }),
         cssPlugin({ generateScopedName: "[folder]__[local]" }),
         rdiPlugin(),
