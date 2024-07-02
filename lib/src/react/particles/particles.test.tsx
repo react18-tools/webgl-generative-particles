7a8821d0e9988ed07f97568b31a0cc94ff903082
Mayank <mayank.srmu@gmail.com>
2024-07-02 14:01:45 +0530

    Update tests

--- lib/src/react/particles/particles.test.tsx
+++ lib/src/react/particles/particles.test.tsx
@@ -2,12 +2,17 @@ import { cleanup, render, screen } from "@testing-library/react";
 import { afterEach, describe, test } from "vitest";
 import { Particles } from "./particles";
 
-describe.concurrent("particles", () => {
-	afterEach(cleanup);
+describe("particles", () => {
+  afterEach(cleanup);
 
-	test("Dummy test - test if renders without errors", ({ expect }) => {
-		const clx = "my-class";
-		render(<Particles className={clx} />);
-		expect(screen.getByTestId("particles").classList).toContain(clx);
-	});
+  test("Test if renders without errors", ({ expect }) => {
+    const clx = "my-class";
+    render(<Particles className={clx} />);
+    expect(screen.getByTestId("particles").classList).toContain(clx);
+  });
+
+  test("Test fullScreen overlay mode", ({ expect }) => {
+    render(<Particles fullScreenOverlay />);
+    expect(screen.getByTestId("particles").style.width).toBe("100vw");
+  });
 });
